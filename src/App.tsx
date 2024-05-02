2a0e4d71c8c7d2a3ed38af50d7334065d04e876b
Jakob Persson <jakper01@easv365.dk>
2024-05-02 16:45:00 +0200

    Experimental commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
 import './App.css';
 
 function App() {
@@ -17,28 +15,11 @@ function App() {
           </ul>
         </nav>
       </header>
-      <div className="container">
-        <div className="logo-container">
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
+      <div className="container" ></div>
         <h1>Welcome to the Pandaverse</h1>
         <div className="image-frame">
-          <img src="placeholder-image.jpg" alt="Placeholder" />
+          <img src="pngegg.png"/>
         </div>
-        <div className="card">
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
     </>
   );
 }
